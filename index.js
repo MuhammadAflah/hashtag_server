d439ea060480fafb6c34467bcc7271a41134d81b
Muhammad Aflah <muhammadaflah786@gmail.com>
2023-03-20 20:41:01 +0530

    all done

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: 'http://localhost:3000'
+        origin: 'https://master.d1nv9eb5rafclu.amplifyapp.com'
     }
 });
 app.use(express.json());
